1cc2da937115a1ed9faec4468aac4eead220f673
Md-Shahzadah-Sattar <148210694+Md-Shahzadah-Sattar@users.noreply.github.com>
2023-12-20 19:15:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function fibonacci(num) {
-// your code here
+  if (num === 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    let a = 0, b = 1;
+    for (let i = 2; i < num; i++) {
+      let temp = a + b;
+      a = b;
+      b = temp;
+    }
+    return b;
+  }
 }
+
+// Test cases
+console.log(fibonacci(1));  // Output: 0
+console.log(fibonacci(5));  // Output: 3
 
 module.exports = fibonacci;
